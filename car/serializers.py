47edf5a7347e409e1d661f5aea5a2267c6392d64
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-17 21:34:49 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,47 @@
 from rest_framework import serializers
+from django.core.validators import MaxValueValidator, MinValueValidator
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[
+            MaxValueValidator(1914),
+            MinValueValidator(1)
+        ]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        allow_null=True,
+        required=False
+    )
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get(
+            "model",
+            instance.manufacturer
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.manufacturer
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.manufacturer
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.manufacturer
+        )
+        instance.save()
+        return instance
--- car/views.py
+++ car/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+
+from car.models import Car
+from car.serializers import CarSerializer
+
+
+@api_view(["GET"])
+def car_list(request):
+    if request.method == "GET":
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def car_detail(request, pk):
+    if request.method == "GET":
+        car = get_object_or_404(Car, pk=pk)
+        serializer = CarSerializer(car)
+        return Response(serializer.data, status=status.HTTP_200_OK)
