0b0707e6c59626c543b9cd555e1c5d9230e0faf7
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-08-17 21:37:22 +0200

    solution v2

--- car_service/urls.py
+++ car_service/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from car.views import car_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
